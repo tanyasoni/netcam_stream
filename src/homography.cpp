533ece8bd8f63488f5b9c1d2831e92ec683ce108
GreatAlexander <alex.bordallo@gmail.com>
2015-12-01 17:11:40 +0000

    Delete params on node shutdown, fix code layout
     - Both netcam main and homography delete parameter namespace to keep things clean
    
    Signed-off-by: GreatAlexander <alex.bordallo@gmail.com>

--- src/homography.cpp
+++ src/homography.cpp
@@ -8,7 +8,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
 #include <sensor_msgs/CameraInfo.h>
-#include <camera_info_manager/camera_info_manager.h> 
+#include <camera_info_manager/camera_info_manager.h>
 #include <ros/console.h>
 #include <ros/package.h>
 #include <iostream>
@@ -16,16 +16,15 @@
 using namespace cv;
 using namespace ros;
 
-int CAMERA_ID; 
-Size BOARD_SIZE; 
-double SQUARE_SIZE; 
+int CAMERA_ID;
+Size BOARD_SIZE;
+double SQUARE_SIZE;
 Point2f BOARD_OFFSET;
 
 template <typename T>
-std::string to_str(T a)
-{
+std::string to_str(T a) {
     std::ostringstream temp;
-    temp<<a;
+    temp << a;
     return temp.str();
 }
 
@@ -36,23 +35,25 @@ int x_coeff, y_coeff;
 bool new_image = false;
 
 
-static void calcChessboardCorners2D(Size boardSize, float squareSize, vector<Point2f>& corners)
-{
+static void calcChessboardCorners2D(Size boardSize, float squareSize,
+                                    vector<Point2f>& corners) {
     corners.resize(0);
-    for( int i = 0; i < boardSize.height; i++ )
-        for( int j = 0; j < boardSize.width; j++ )
-            corners.push_back(Point2f(x_coeff * float(j*squareSize),
-                                      y_coeff * float(i*squareSize)) + BOARD_OFFSET);
+    for ( int i = 0; i < boardSize.height; i++ )
+        for ( int j = 0; j < boardSize.width; j++ )
+            corners.push_back(Point2f(x_coeff * float(j * squareSize),
+                                      y_coeff * float(i * squareSize)) + BOARD_OFFSET);
 }
 
-void calc_homography(Mat& image)
-{
+void calc_homography(Mat& image) {
     vector<Point2f> real_corners;
     calcChessboardCorners2D(BOARD_SIZE, SQUARE_SIZE, real_corners);
 
     vector<Point2f> pointBuf;
     bool found = findChessboardCorners(image, BOARD_SIZE, pointBuf,
-                CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_FAST_CHECK  | CV_CALIB_CB_FILTER_QUADS); // CALIB_CB_NORMALIZE_IMAGE
+                                       CALIB_CB_ADAPTIVE_THRESH |
+                                       CALIB_CB_FAST_CHECK  |
+                                       CV_CALIB_CB_FILTER_QUADS);
+    // CALIB_CB_NORMALIZE_IMAGE
 
     // if (found)
     // {
@@ -65,10 +66,11 @@ void calc_homography(Mat& image)
     drawChessboardCorners(image, BOARD_SIZE, Mat(pointBuf), found);
     ROS_INFO("[CALC_HOMOGRAPHY] Found board: %d", found);
 
-    if (found)
-    {
-        ROS_DEBUG_STREAM("Number of calc. points: " << pointBuf.size() << std::endl << pointBuf << std::endl);
-        ROS_DEBUG_STREAM("Number of found points: " << real_corners.size() << std::endl << real_corners << std::endl);
+    if (found) {
+        ROS_DEBUG_STREAM("Number of calc. points: " << pointBuf.size() << std::endl <<
+                         pointBuf << std::endl);
+        ROS_DEBUG_STREAM("Number of found points: " << real_corners.size() << std::endl
+                         << real_corners << std::endl);
 
         H = cv::findHomography(pointBuf, real_corners);
 
@@ -76,7 +78,8 @@ void calc_homography(Mat& image)
 
         // Save to file
         std::string path = ros::package::getPath("netcam_stream");
-        FileStorage fs(path + "/calibration/homography" + to_str(CAMERA_ID) + ".yaml", FileStorage::WRITE);
+        FileStorage fs(path + "/calibration/homography" + to_str(CAMERA_ID) + ".yaml",
+                       FileStorage::WRITE);
         fs << "H_" + to_str(CAMERA_ID) << H;
         fs.release();
 
@@ -84,7 +87,7 @@ void calc_homography(Mat& image)
         for (int i = 0; i < 3; ++i)
             for (int j = 0; j < 3; ++j)
                 std::cout << "H" << i << j << " " << H.at<double>(i, j) << std::endl;
-            
+
     }
     imshow("Calc Board", image);
     waitKey(10);
@@ -92,24 +95,21 @@ void calc_homography(Mat& image)
 }
 
 
-void imageCb(const sensor_msgs::ImageConstPtr& msg)
-{
+void imageCb(const sensor_msgs::ImageConstPtr& msg) {
     cv_bridge::CvImagePtr cv_ptr;
     cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
     frame = cv_ptr->image;
     new_image = true;
 }
 
-void camera_info(const sensor_msgs::CameraInfo::ConstPtr& msg)
-{
-    if (!got_cam_params)
-    {
+void camera_info(const sensor_msgs::CameraInfo::ConstPtr& msg) {
+    if (!got_cam_params) {
         ROS_INFO("[CALC_HOMOGRAPHY] Camera info has been received.");
         got_cam_params = true;
         K = Mat::ones(3, 3, CV_64F);
         for (int i = 0; i < 3; ++i)
             for (int j = 0; j < 3; ++j)
-                K.at<double>(i,j) = msg->K[i*3+j];
+                K.at<double>(i, j) = msg->K[i * 3 + j];
         distcoeff = Mat::ones(5, 1, CV_64F);
         for (int i = 0; i < 5; ++i) distcoeff.at<double>(i, 1) = msg->D[i];
         ROS_INFO_STREAM("K: " << K);
@@ -117,16 +117,17 @@ void camera_info(const sensor_msgs::CameraInfo::ConstPtr& msg)
     }
 }
 
-void chessboardMouseCallback(int event, int x, int y, int flags, void* userdata)
-{
-    if(event == EVENT_LBUTTONDOWN)
-    {
-        if (H.empty())
-        {
-            ROS_WARN("[CALC_HOMOGRAPHY] You need to find chessboard first. Use 'h'.");
+void chessboardMouseCallback(int event, int x, int y, int flags,
+                             void* userdata) {
+    if (event == EVENT_LBUTTONDOWN) {
+        if (H.empty()) {
+            ROS_WARN(
+                "[CALC_HOMOGRAPHY] You need to find chessboard first. Use 'h'.");
             return;
         }
-        ROS_INFO_STREAM("[CALC_HOMOGRAPHY] Left button of the mouse is clicked - position (" << x << ", " << y << ")");
+        ROS_INFO_STREAM(
+            "[CALC_HOMOGRAPHY] Left button of the mouse is clicked - position ("
+            << x << ", " << y << ")");
 
         std::vector<Point2f> img_pnt(1, Point2f(x, y)), scene_pnt(1);
 
@@ -136,23 +137,21 @@ void chessboardMouseCallback(int event, int x, int y, int flags, void* userdata)
     }
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     ros::init(argc, argv, "calc_homography");
-    if (!ros::master::check())
-    {
+    if (!ros::master::check()) {
         ROS_ERROR("[CALC_HOMOGRAPHY] Cannot detect ROS master!");
         return 1;
     }
-    
-    ros::NodeHandle node("~"); 
+
+    ros::NodeHandle node("~");
     // Get params
     node.getParam("camera_id", CAMERA_ID);
     int w, h;
     node.getParam("board_size_w", w); node.getParam("board_size_h", h);
     BOARD_SIZE = Size(w, h);
     node.getParam("square_size", SQUARE_SIZE);
-    double x, y;  
+    double x, y;
     node.getParam("board_offset_x", x); node.getParam("board_offset_y", y);
     BOARD_OFFSET = Point2f(x, y);
     node.getParam("x_coeff", x_coeff); node.getParam("y_coeff", y_coeff);
@@ -160,23 +159,28 @@ int main(int argc, char** argv)
     // Set up topics
     ROS_INFO("[CALC_HOMOGRAPHY] Setting up topics.");
     image_transport::ImageTransport it(node);
-    image_transport::Subscriber sub = it.subscribe("/netcam_stream_" + to_str(CAMERA_ID) + "/image_rect_color", 1, &imageCb);
-    ros::Subscriber cam_info_sub =  node.subscribe("/netcam_stream_" + to_str(CAMERA_ID) + "/camera_info", 1, &camera_info);
+    image_transport::Subscriber sub =
+        it.subscribe("/netcam_stream_" +
+                     to_str(CAMERA_ID) + "/image_rect_color", 1, &imageCb);
+    ros::Subscriber cam_info_sub =
+        node.subscribe("/netcam_stream_" +
+                       to_str(CAMERA_ID) + "/camera_info", 1, &camera_info);
 
     ros::Rate loop_rate(30);
     unsigned int frame_id = 0;
 
     namedWindow("Board");
     setMouseCallback("Board", chessboardMouseCallback, NULL);
-    ROS_INFO("[CALC_HOMOGRAPHY] Place the calibration board on the desired position (described by the offset params) "
-                "and press 'h' on the image to calculate the homography. Re-press 'h' to re-attempt.");
+    ROS_INFO(
+        "[CALC_HOMOGRAPHY] Place the calibration board on the desired position (described by the offset params) "
+        "and press 'h' on the image to calculate the homography. Re-press 'h' to re-attempt.");
     while (node.ok()) {
-        if (to_process){
+        if (to_process) {
             calc_homography(frame);
             to_process = false;
         }
-        
-        if (new_image){
+
+        if (new_image) {
             imshow("Board", frame);
             new_image = false;
         }
@@ -184,11 +188,12 @@ int main(int argc, char** argv)
         char k = waitKey(10);
         if (k == 'h')
             to_process = true;
-        
+
         ros::spinOnce();
         loop_rate.sleep();
     }
 
+    ros::param::del("~");
     ros::shutdown();
     return 0;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -14,18 +14,16 @@
 using namespace cv;
 using namespace ros;
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     ros::init(argc, argv, "netcam_stream");
 
-    if (!ros::master::check())
-    {
+    if (!ros::master::check()) {
         ROS_ERROR("[NETCAM_STREAM] Cannot detect ROS master!");
         return 1;
     }
     ROS_INFO("[NETCAM_STREAM] Advertising.");
 
-    ros::NodeHandle node("~");  
+    ros::NodeHandle node("~");
 
     int id; std::string CAMERA_ID;
     if (!node.getParam("camera_id", id))
@@ -35,16 +33,18 @@ int main(int argc, char** argv)
     CAMERA_ID = ss.str();
 
     std::string user, pass;
-    if (!node.getParam("/cam_user", user)){
-        if (!node.getParam("/launch_user", user) || (user == "user")){
-            ROS_ERROR("[NETCAM_STREAM] Cannot read username from param server (cam_user) or launch file is not changed (launch_user).");
+    if (!node.getParam("/cam_user", user)) {
+        if (!node.getParam("/launch_user", user) || (user == "user")) {
+            ROS_ERROR(
+                "[NETCAM_STREAM] Cannot read username from param server (cam_user) or launch file is not changed (launch_user).");
             return 1;
         }
         ROS_INFO("The user is: %s", user.c_str());
     }
-    if (!node.getParam("/cam_pass", pass)){
-        if (!node.getParam("/launch_pass", pass) || (pass == "pass")){
-            ROS_ERROR("[NETCAM_STREAM] Cannot read password from param server (cam_pass) or launch file is not changed (launch_pass).");
+    if (!node.getParam("/cam_pass", pass)) {
+        if (!node.getParam("/launch_pass", pass) || (pass == "pass")) {
+            ROS_ERROR(
+                "[NETCAM_STREAM] Cannot read password from param server (cam_pass) or launch file is not changed (launch_pass).");
             return 1;
         }
     }
@@ -53,38 +53,48 @@ int main(int argc, char** argv)
     image_transport::Publisher pub = it.advertise("image_raw", 1);
     std::string path = ros::package::getPath("netcam_stream");
     ROS_INFO("[NETCAM_STREAM] Path to calibration files: %s", path.c_str());
-    camera_info_manager::CameraInfoManager cim(node, "netcam_stream_" + CAMERA_ID, "file://" + path + "/calibration/cam"+ CAMERA_ID + ".yaml");
-    
-    ros::Publisher pub_info = node.advertise<sensor_msgs::CameraInfo>("camera_info", 1);
+    camera_info_manager::CameraInfoManager cim(
+        node, "netcam_stream_" + CAMERA_ID,
+        "file://" + path + "/calibration/cam" + CAMERA_ID + ".yaml");
+
+    ros::Publisher pub_info = node.advertise<sensor_msgs::CameraInfo>(
+                                  "camera_info", 1);
 
 
     if (cim.isCalibrated())
         ROS_INFO("[NETCAM_STREAM] Camera is calibrated!");
 
     cv::VideoCapture net_cam;
-    bool isOpened = net_cam.open("http://" + user + ":" + pass + "@192.168.107." + CAMERA_ID + "/mjpg/video.mjpg"); 
-    if (!isOpened){
-        ROS_ERROR("[NETCAM_STREAM] Network camera stream is not opened! Have you checked the username and password. Trying to access '%s'", ("http://" + user + ":{the_password}@192.168.107." + CAMERA_ID + "/mjpg/video.mjpg").c_str());
+    bool isOpened = net_cam.open(
+                        "http://" + user + ":" + pass + "@192.168.107." +
+                        CAMERA_ID + "/mjpg/video.mjpg");
+    if (!isOpened) {
+        ROS_ERROR(
+            "[NETCAM_STREAM] Network camera stream is not opened! Have you checked the username and password. Trying to access '%s'",
+            ("http://" + user + ":{the_password}@192.168.107." + CAMERA_ID +
+             "/mjpg/video.mjpg").c_str());
         return 1;
     }
 
-    ros::Rate loop_rate(30); // 30Hz
+    ros::Rate loop_rate(30); //30Hz
     unsigned int frame_id = 0;
     while (node.ok()) {
         ros::spinOnce();
 
         cv::Mat frame;
-        net_cam >> frame;   
-        if (frame.empty()){
+        net_cam >> frame;
+        if (frame.empty()) {
             ROS_WARN("[NETCAM_STREAM] Camera returned empty frame! Skipping...");
             continue;
         }
         ++frame_id;
-        sensor_msgs::ImagePtr image = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
-        
+        sensor_msgs::ImagePtr image = cv_bridge::CvImage(std_msgs::Header(), "bgr8",
+                                                         frame).toImageMsg();
+
         image->header.frame_id = frame_id;
         // Get current CameraInfo data
-        sensor_msgs::CameraInfoPtr ci(new sensor_msgs::CameraInfo(cim.getCameraInfo()));
+        sensor_msgs::CameraInfoPtr ci(
+            new sensor_msgs::CameraInfo(cim.getCameraInfo()));
         ci->header.frame_id = frame_id;
         ci->header.stamp = image->header.stamp;
 
@@ -94,7 +104,7 @@ int main(int argc, char** argv)
         loop_rate.sleep();
     }
 
-    
+    ros::param::del("~");
     ros::shutdown();
     return 0;
 }
