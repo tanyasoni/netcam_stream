6b9f44dde84497f2806964d558fea369b360e444
Daniel Angelov <dani_angelov@yahoo.com>
2016-01-12 16:48:13 +0000

    Added frame_rate parameter to specify publisher Hz. Logging now has camera id.

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,6 @@
 
 #include <signal.h>
 
-
-
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
@@ -22,13 +20,15 @@
 using namespace cv;
 using namespace ros;
 
+bool to_publish = false;
+
 void clean_up_gracefully(std::string id = "")
 {
     ros::param::del("~");
     ros::shutdown();
     if (id.size() != 0){
-        ROS_INFO("Attempting to kill image_proc in 2 sec.");
-        ros::Duration(2).sleep(); // Wait before killing the node
+        ROS_INFO("[NETCAM_STREAM_%s] Attempting to kill image_proc in 2 sec.", id.c_str());
+        ros::Duration(5).sleep(); // Wait before killing the node
         system(("rosnode kill netcam_stream_" + id + "/image_proc_" + id).c_str());
     }
 }
@@ -36,7 +36,7 @@ void clean_up_gracefully(std::string id = "")
 // Replacement SIGINT handler
 void mySigIntHandler(int sig)
 {
-    ROS_WARN("Shutdown request received. Reason id: %d", sig);
+    ROS_WARN("[NETCAM_STREAM] Shutdown request received. Reason id: %d", sig);
     clean_up_gracefully();
 }
 
@@ -49,11 +49,16 @@ void shutdownCallback(XmlRpc::XmlRpcValue& params, XmlRpc::XmlRpcValue& result)
     if (num_params > 1)
     {
         std::string reason = params[1];
-        ROS_WARN("Shutdown request received. Reason: [%s]", reason.c_str());
+        ROS_WARN("[NETCAM_STREAM] Shutdown request received. Reason: [%s]", reason.c_str());
         clean_up_gracefully();
     }
 
     result = ros::xmlrpc::responseInt(1, "", 0);
+}
+
+void publish_image_callback(const ros::TimerEvent&)
+{
+    to_publish = true;
 }
 
 
@@ -89,7 +94,7 @@ int main(int argc, char** argv) {
     if (!node.getParam("/cam_user", user)) {
         if (!node.getParam("/launch_user", user) || (user == "user")) {
             ROS_FATAL(
-                "[NETCAM_STREAM] Cannot read username from param server (cam_user) or launch file is not changed (launch_user).");
+                "[NETCAM_STREAM_%d] Cannot read username from param server (cam_user) or launch file is not changed (launch_user).", id);
             clean_up_gracefully(CAMERA_ID);
             return 0;
         }
@@ -98,17 +103,23 @@ int main(int argc, char** argv) {
     if (!node.getParam("/cam_pass", pass)) {
         if (!node.getParam("/launch_pass", pass) || (pass == "pass")) {
             ROS_FATAL(
-                "[NETCAM_STREAM] Cannot read password from param server (cam_pass) or launch file is not changed (launch_pass).");
+                "[NETCAM_STREAM_%d] Cannot read password from param server (cam_pass) or launch file is not changed (launch_pass).", id);
             clean_up_gracefully(CAMERA_ID);
             return 0;
         }
     }
 
-    ROS_INFO("[NETCAM_STREAM] Advertising.");
+    int frame_rate;
+    if (!node.getParam("/frame_rate", frame_rate)){
+        ROS_WARN("[NETCAM_STREAM_%d] Camera frequency ('frame_rate') cannot be read. Using 30 Hz.", id);
+        frame_rate = 30;
+    }
+
+    ROS_INFO("[NETCAM_STREAM_%d] Advertising.", id);
     image_transport::ImageTransport it(node);
     image_transport::Publisher pub = it.advertise("image_raw", 1);
     std::string path = ros::package::getPath("netcam_stream");
-    ROS_INFO("[NETCAM_STREAM] Path to calibration files: %s", path.c_str());
+    ROS_INFO("[NETCAM_STREAM_%d] Path to calibration files: %s", id, path.c_str());
     camera_info_manager::CameraInfoManager cim(
         node, "netcam_stream_" + CAMERA_ID,
         "file://" + path + "/calibration/cam" + CAMERA_ID + ".yaml");
@@ -118,7 +129,9 @@ int main(int argc, char** argv) {
 
 
     if (cim.isCalibrated())
-        ROS_INFO("[NETCAM_STREAM] Camera is calibrated!");
+        ROS_INFO("[NETCAM_STREAM_%d] Camera is calibrated.", id);
+    else 
+        ROS_WARN("[NETCAM_STREAM_%d] Camera is NOT calibrated!", id);
 
     std::string cam_url = "http://" + user + ":" + pass + "@192.168.107." +
                           CAMERA_ID + "/mjpg/video.mjpg";
@@ -126,14 +139,15 @@ int main(int argc, char** argv) {
     bool isOpened = net_cam.open(cam_url);
     if (!isOpened) {
         ROS_FATAL(
-            "[NETCAM_STREAM] Network camera stream is not opened! Have you checked the username and password. Trying to access '%s'",
-            ("http://" + user + ":{the_password}@192.168.107." + CAMERA_ID +
-             "/mjpg/video.mjpg").c_str());
+            "[NETCAM_STREAM_%d] Network camera stream is not opened! Have you checked the username and password. Trying to access '%s'", 
+            id, ("http://" + user + ":{the_password}@192.168.107." + CAMERA_ID +
+                 "/mjpg/video.mjpg").c_str());
         clean_up_gracefully(CAMERA_ID);
         return 0;
     }
 
-    ros::Rate loop_rate(30); // 30Hz
+    ros::Rate loop_rate(30);
+    ros::Timer timer = node.createTimer(ros::Duration(1.0/frame_rate), publish_image_callback);
     while (node.ok()) {
         ros::spinOnce();
 
@@ -146,26 +160,31 @@ int main(int argc, char** argv) {
         cv::Mat frame;
         net_cam >> frame;
         if (frame.empty()) {
-            ROS_WARN("[NETCAM_STREAM] Camera returned empty frame! Skipping.");
-            ROS_INFO("[NETCAM_STREAM] Reopening stream...");
+            ROS_WARN("[NETCAM_STREAM_%d] Camera returned empty frame! Skipping.", id);
+            ROS_INFO("[NETCAM_STREAM_%d] Reopening stream...", id);
 
             if (!net_cam.open(cam_url))
-                ROS_FATAL("[NETCAM_STREAM] Network camera stream cannot be opened!");
+                ROS_FATAL("[NETCAM_STREAM_%d] Network camera stream cannot be opened!", id);
             continue;
         }
-        sensor_msgs::ImagePtr image = cv_bridge::CvImage(std_msgs::Header(), "bgr8",
-                                                         frame).toImageMsg();
 
-        std::string frame_id = "cam_" + CAMERA_ID;
-        image->header.frame_id = frame_id;
-        // Get current CameraInfo data
-        sensor_msgs::CameraInfoPtr ci(
-            new sensor_msgs::CameraInfo(cim.getCameraInfo()));
-        ci->header.frame_id = frame_id;
-        ci->header.stamp = image->header.stamp;
+        if (to_publish){
+            sensor_msgs::ImagePtr image = cv_bridge::CvImage(std_msgs::Header(), "bgr8",
+                                                             frame).toImageMsg();
 
-        pub.publish(image);
-        pub_info.publish(ci);
+            std::string frame_id = "cam_" + CAMERA_ID;
+            image->header.frame_id = frame_id;
+            // Get current CameraInfo data
+            sensor_msgs::CameraInfoPtr ci(
+                new sensor_msgs::CameraInfo(cim.getCameraInfo()));
+            ci->header.frame_id = frame_id;
+            ci->header.stamp = image->header.stamp;
+
+            pub.publish(image);
+            pub_info.publish(ci);
+
+            to_publish = false;
+        }
 
         loop_rate.sleep();
     }
